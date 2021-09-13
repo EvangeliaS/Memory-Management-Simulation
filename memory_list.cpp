ad257fe01839c13cc9e21ba927cefc9916fe9f25
EvangeliaS <evangelia.s@hotmail.gr>
2021-09-13 04:43:56 +0300

    11

--- memory_list.cpp
+++ memory_list.cpp
@@ -3,3 +3,36 @@
 
 using namespace std;
 
+Memory_List_Node::Memory_List_Node(int s, int x, int y, Process* proc){
+    this->size  = s;
+    this->start = x;
+    this->end   = y;
+    this->process = proc;
+    this->next = NULL;
+    this->prev = NULL;
+}
+
+Memory_List_Node::~Memory_List_Node(){
+    delete this->process;
+    this->process = NULL;
+    this->next = NULL;
+    this->prev = NULL;
+}
+
+Memory_List::Memory_List(){
+    this->head = NULL;
+    this->tail = NULL;
+}
+
+Memory_List::~Memory_List(){
+    Memory_List_Node* prev = this->head;
+    Memory_List_Node* temp = prev->next;
+
+    while(prev){
+        delete prev;
+        prev = temp;
+        temp = temp->next;
+    }
+    this->head = NULL;
+    this->tail = NULL;
+}
--- memory_list.hpp
+++ memory_list.hpp
@@ -8,13 +8,12 @@ class Memory_List_Node{
         int start;
         int end;
         int size;
-
         Process* process;
     public:
         Memory_List_Node* next;
         Memory_List_Node* prev;
 
-        Memory_List_Node(int s, int x, int y);
+        Memory_List_Node(int s, int x, int y, Process*);
         ~Memory_List_Node();
         inline Process* get_process(){
             return this->process;
@@ -32,6 +31,8 @@ class Memory_List{
         bool spot_empty_block();
         int best_fit(Process* process);
         int worst_fit(Process* process);
+        int insert_node(Process* proc);
+        int delete_node(Process* proc);
         
         inline Memory_List_Node* get_head(){
             return this->head;
